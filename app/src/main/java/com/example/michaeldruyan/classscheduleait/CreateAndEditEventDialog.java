b58c262e3bd10cbdf115d7be9f215df0395ea7cc
awilli1025 <akwilliams19@amherst.edu>
2018-05-16 21:48:53 +0200

    dialog layout xml

--- app/src/main/java/com/example/michaeldruyan/classscheduleait/CreateAndEditEventDialog.java
+++ app/src/main/java/com/example/michaeldruyan/classscheduleait/CreateAndEditEventDialog.java
@@ -1,9 +1,21 @@
 package com.example.michaeldruyan.classscheduleait;
 
+import android.support.v4.app.DialogFragment;
+import android.widget.EditText;
+
+import com.example.michaeldruyan.classscheduleait.data.Event;
+
 /**
  * Created by Patrick on 5/16/18.
  */
 
-public class CreateAndEditEventDialog {
+public class CreateAndEditEventDialog extends DialogFragment {
+    public interface ItemHandler {
+        public void onNewItemCreated(Event event);
 
+        public void onItemUpdated(Event event);
+    }
+
+    private ItemHandler itemHandler;
+    private EditText
 }
